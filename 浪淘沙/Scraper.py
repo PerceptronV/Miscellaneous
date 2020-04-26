694906599e17c0a83dd604e48665a2221067fb64
PerceptronV <neutrinovs@gmail.com>
2020-04-26 17:47:36 +0800

    Update Scraper.py

--- 浪淘沙/Scraper.py
+++ 浪淘沙/Scraper.py
@@ -1,2 +1,6 @@
 import bs4
 import requests
+url='https://github.com/chinese-poetry/chinese-poetry/tree/master/json'
+page=requests.get(url)
+soup=bs4.BeautifulSoup(page.text,'html.parser')
+soup.find_all()
